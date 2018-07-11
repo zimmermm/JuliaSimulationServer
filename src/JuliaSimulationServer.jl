f5a4205fb8df756fe6ed05f22c2ffeedf377182f
Matthias Zimmermann <zimmermm@hotmail.com>
2018-07-11 13:19:59 +0200

    basic version

--- src/JuliaSimulationServer.jl
+++ src/JuliaSimulationServer.jl
@@ -1,17 +1,15 @@
 __precompile__()
 module JuliaSimulationServer
 # import heavy libraries
-using OrdinaryDiffEq
+using DifferentialEquations
+using ODEInterfaceDiffEq
+using StaticArrays
 using Interpolations: interpolate, Gridded, Linear
 using Calculus: derivative
 using DataFrames
 using CSV
 using ConvectionBoxmodel
-using ConvectionBoxmodel.ConvectionBoxmodelToolbox
-
-function compute(x::Float64)
-	@time x^2
-end
+using rotsee_boxmodel
 
 function start()
 	start(pwd())
@@ -36,21 +34,39 @@ function job_done(path::String)
 	close(f)
 end
 
+function job_failed(path::String)
+	f = open(path, "w")
+	write(f, "failed")
+	close(f)
+end
+
 function start(workspace::String)
 	srv_queue_path = joinpath(workspace, "simulation_server_queue.jss")
-	keywords = ["stop", "done"]
+	keywords = ["stop", "done", "failed"]
 	touch(srv_queue_path)
 	stop_server = false
 	println("Simulation server up...")
 	while ~stop_server
-		watch_file(srv_queue_path)
-		lines = read_queue(srv_queue_path)
-		if length(lines) > 0
-		   	stop_server = lines[1] == "stop"
-		   	if ~(lines[1] in keywords)
-		   		@time eval(parse(lines[1]))
-		   		job_done(srv_queue_path)
-		   	end
+		try
+			watch_file(srv_queue_path)
+			lines = read_queue(srv_queue_path)
+			if length(lines) > 0
+			   	stop_server = lines[1] == "stop"
+			   	if ~(lines[1] in keywords)
+			   		println("Start job: ", lines[1])
+			   		try
+			   			@time eval(parse(lines[1]))
+			   			job_done(srv_queue_path)
+						println("[Done]")
+			   		catch e
+			   			bt = catch_backtrace()                                                                                            
+	           			showerror(STDERR, e, bt)
+			   			job_failed(srv_queue_path)
+			   		end
+			   	end
+			end
+		catch
+			sleep(1)
 		end
 	end
 	sleep(1)
